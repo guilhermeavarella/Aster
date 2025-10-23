b107ff9ff1469841dbc79f5e59d1dcabd34faf61
leticia-noh <leticianhonda@gmail.com>
2025-10-22 23:06:29 -0300

    chore: criação do projeto base do back-end

---
+++ backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/AsterDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aster.aster_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsterDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/aster-dashboard-backend/src/test/java/com/aster/aster_dashboard_backend/AsterDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aster.aster_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
