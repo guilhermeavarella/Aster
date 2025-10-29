c89d767a9c40d0f2a70807d43a39ab96d1c8b31d
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-29 10:45:45 -0300

    fix: tailwind usage fix

--- frontend/aster-frontend/src/App.tsx
+++ frontend/aster-frontend/src/App.tsx
@@ -5,26 +5,10 @@ import LiquidGlass from 'liquid-glass-react'
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>This is H1. Lorem Ipsum</h1>
-      <h2>This is H2. Lorem Ipsum</h2>
-      <h3>This is H3. Lorem Ipsum</h3>
-      <h4>This is H4. Lorem Ipsum</h4>
-      <p>This is a paragraph. Lorem Ipsum</p>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>  
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen min-w-screen flex flex-row items-start justify-start bg-background-aurora">
+      <section className="m-10 p-10 rounded-lg text-white bg-black"> Teste </section>
+      <section> Teste </section>
+    </div>
   )
 }
 
