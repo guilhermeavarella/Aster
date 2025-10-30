80a35e4a6ef275bdc5ee04e14ca717004dfea05c
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-10-30 08:54:30 -0300

    chore: new glass lib

--- frontend/aster-frontend/src/App.tsx
+++ frontend/aster-frontend/src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import LiquidGlass from 'liquid-glass-react'
+import { LiquidGlassReact } from 'solid-glass/react';
+import "solid-glass/style.css";
 
 
 function App() {  
   return (
-    <div className="min-h-screen min-w-screen flex flex-row items-start justify-start">  
-      <section className="m-10 p-10 rounded-lg text-white bg-content-primary"> Teste </section>
-      <section> Teste </section>
+    <div className="w-full h-full min-h-[200vh] min-w-screen flex flex-row items-start justify-start bg-gradient-to-bl from-[var(--brand-pink)] via-[var(--brand-lavender)] to-[var(--brand-blue)]">  
+      <div className="w-full h-full max-w-92 flex flex-row items-start justify-center p-6">
+        <LiquidGlassReact
+      width="400px"
+      height="300px"
+      tintColor="[var(--fixed-white)]"
+      tintOpacity={0.05}
+      frostBlur={50}
+      className="p-6"
+    >
+      <h1>Your Content Here</h1>
+    </LiquidGlassReact>
+      </div>
+      <div className="w-full h-full pt-6 pb-6 pl-3 pr-9">
+
+      </div>
     </div>
   )
 }
