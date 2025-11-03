de7fd86416fbc70451406dce8856d0516617f676
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-11-03 11:16:39 -0300

    fix: future entity routes

--- frontend/aster-frontend/src/main.tsx
+++ frontend/aster-frontend/src/main.tsx
@@ -19,9 +19,9 @@ const router = createBrowserRouter([
       { path: 'painel/d/:user', element: <Painel_1 /> },
       { path: 'painel/i/:user', element: <Painel_2 /> },
 
-      { path: 'operacoes/exibir/:entId', element: <Exibir /> },
-      { path: 'operacoes/alterar/:entId', element: <Alterar /> },
-      { path: 'operacoes/criar/:entId?', element: <Criar /> }
+      { path: 'operacoes/exibir/:entidade', element: <Exibir /> },
+      { path: 'operacoes/alterar/:entidade/:regId', element: <Alterar /> },
+      { path: 'operacoes/criar/:entidade/:regId?', element: <Criar /> }
 
       { path: 'suporte/devolutivas', element: <Devolutivas /> },
       { path: 'suporte/responder', element: <Responder /> },*/
