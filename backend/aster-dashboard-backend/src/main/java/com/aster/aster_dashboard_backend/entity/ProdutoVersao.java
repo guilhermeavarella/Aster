504f141987703a44e612ad1e72f3b616cc508013
Kawagui <135202943+Kawagui@users.noreply.github.com>
2025-11-04 19:17:39 -0300

    Refactor ProdutoVersao entity annotations and formatting

--- backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/entity/ProdutoVersao.java
+++ backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/entity/ProdutoVersao.java
@@ -17,22 +17,20 @@ public class ProdutoVersao{
     @EmbeddedId
     private ProdutoVersaoId id;
 
-    @Column(name="arquivo_instalador", length = 50)
+    @Column(name="arquivo_instalador", length=50)
     private String arquivoInstalador;
 
     @Column(name="data_lancamento")
     private LocalDate dataLancamento;
 
-    @Column(name="patch_notes", length = 200)
+    @Column(name="patch_notes", length=200)
     private String patchNotes;
-
-    @MapsId("produtoId")
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "produto_id", nullable = false)
+    
+    @Column(name="produto_id", nullable=false)
     private Produto produto;
 
     @Override
-    public String toString() {
+    public String toString(){
         return "ProdutoVersao{" +
                 "numeroVersao='" + id.getNumeroVersao + '\'' +
                 ", produtoId='" + id.getProdutoId + '\'' +
