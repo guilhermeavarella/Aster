65028e24555e89dc226aef8fc2a6ea1c9102f1f0
leticia-noh <leticianhonda@gmail.com>
2025-11-12 23:08:36 -0300

    feat: add lastPage to PageResponseDto

--- backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/dto/PageResponseDto.java
+++ backend/aster-dashboard-backend/src/main/java/com/aster/aster_dashboard_backend/dto/PageResponseDto.java
@@ -16,11 +16,13 @@ public class PageResponseDto<T> {
 
     private int pageNumber;
     private long totalEntries;
+    private int lastPage;
     private List<T> content;
 
     public PageResponseDto(Page<T> page) {
         this.pageNumber = page.getNumber();
         this.totalEntries = page.getTotalElements();
+        this.lastPage = page.getTotalPages() - 1;
         this.content = page.getContent();
     }
 }
