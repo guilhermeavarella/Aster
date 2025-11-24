adcf7a6561efee4777851f12a506c69d0d4dbb7c
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2025-11-24 18:34:14 -0300

    feat: router

--- website/aster-website/src/App.tsx
+++ website/aster-website/src/App.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from 'react-router-dom'
+
 function App() {
 
   return (
-    <div className="w-full h-full min-h-screen min-w-screen flex flex-row items-start justify-start">
-      <h1>Aster Website</h1>
+    <div className="w-full h-full min-h-screen min-w-screen flex flex-row items-start justify-center">
+      <Outlet />
     </div>
   )
 }
--- website/aster-website/src/main.tsx
+++ website/aster-website/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'home', element: <Home /> },/*
+      { path: 'produtos' , element: <Produtos /> },
+      { path: 'sobre', element: <SobreNos /> },
+      { path: 'contato', element: <Contato /> }*/
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
 )
